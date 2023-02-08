f8786f6fed1372b10b71650c5136e35c3b24cb3c
GOKUL_R_CSE <gokulr.cse2020@citchennai.net>
2023-02-08 23:29:53 +0530

    crud

--- src/App.vue
+++ src/App.vue
@@ -24,32 +24,58 @@ export default {
       showAddTask: false
     }
   },
-  created(){
-      this.tasks = [
-        {
-          id: 1,
-          text: 'Doctors Appointment',
-          day: 'March 1st at 2.30pm',
-          reminder: true
-        },
-      ]
-    },
+
     methods: {
-      addTask(task){
-        this.tasks = [...this.tasks, task]
+      async fetchTasks(){
+        const res = await fetch('http://localhost:5000/tasks')
+        const data = await res.json()
+        return data
       },
-      deleteTask(id){
+      async fetchTask(id){
+        const res = await fetch(`http://localhost:5000/tasks/${id}`)
+        const data = await res.json()
+        return data
+      },
+      async addTask(task) {
+      const res = await fetch('http://localhost:5000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      })
+      const data = await res.json()
+      this.tasks = [...this.tasks, data]
+    },
+      async deleteTask(id){
         if(confirm('Are you sure')){
-          this.tasks = this.tasks.filter((task) => task.id !== id)
+          const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'DELETE',
+          })
+          res.status === 200 ? this.tasks = this.tasks.filter((task) => task.id !== id) : alert('Error deleting task')
         }
       },
-      toggleReminder(id){
-        this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+      async toggleReminder(id){
+        const taskToToggle = await this.fetchTask(id)
+        const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(updateTask)
+        })
+        const data = await res.json()
+        this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
       },
       toggleAddTask(){
         this.showAddTask = !this.showAddTask
       }
-    }
+    },
+      async created(){
+      this.tasks = await this.fetchTasks()
+    },
 }
 </script>
 
